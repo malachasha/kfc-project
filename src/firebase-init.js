c84508e20cafccdfc8a4e7c9d227d3f4e50a065a
malachasha <68114464+malachasha@users.noreply.github.com>
2024-04-15 15:53:46 +0300

    created dynamically updating products page
    added default image for products

--- src/firebase-init.js
+++ src/firebase-init.js
@@ -1,8 +1,8 @@
 // firebase-init.js
 import { initializeApp } from "https://cdn.skypack.dev/firebase/app.js";
 import { getFirestore, collection, getDocs, addDoc } from "https://cdn.skypack.dev/firebase/firestore.js";
-
-  // Firebase configuration
+  
+// Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyBSKt1tz9oNcqzOwHB8dUaUe_g234b2blY",
     authDomain: "kfc-project-51e7f.firebaseapp.com",
@@ -25,6 +25,58 @@ querySnapshot.forEach((doc) => {
   console.log(JSON.stringify(doc.data(), null, 2)); // Convert to JSON string with 2-space indentation
   console.log("-------------------------------------------");
 });
+
+// Function to fetch products from Firestore and render them on the webpage
+function displayProducts() {
+  const productsContainer = document.getElementById("products-container");
+
+  // Clear existing products
+  productsContainer.innerHTML = "";
+
+  // Fetch products from Firestore
+  
+    querySnapshot.forEach((doc) => {
+      const product = doc.data();
+
+      // Create HTML elements for the product
+      const productDiv = document.createElement("div");
+      productDiv.classList.add("row", "py-5", "mb-5", "products-list");
+
+      // Set default image URL
+      const defaultImageUrl = "/img/default.png";
+
+      productDiv.innerHTML = `
+        <div class="col-sm-6 product-img">
+        <img src="${product.ImageURL ? product.ImageURL : defaultImageUrl}" class="img-fluid w-300 rounded" alt="${product.Name}" />
+        </div>
+        <div class="col-sm-6">
+           <h3><strong>${product.Name}</strong></h3>
+           <ul>
+              <li>${product.Shape}</li>
+              <li>${product.Description}</li>
+              <li>${product.Blasting}</li>
+              <li>${product.Availability}</li>
+              ${product.Specifications ? `<li>${product.Specifications}</li>` : ''}
+           </ul>     
+          <h3><strong>Sizes</strong></h3>
+          <ul>
+          ${product.Sizes && Array.isArray(product.Sizes) ? product.Sizes.map(size => `<li>${size}</li>`).join("") : "Standart sizes available"}
+          </ul>
+           
+        </div>
+      `;
+
+      // Append the product HTML to the container
+      productsContainer.appendChild(productDiv);
+    });
+  }
+
+
+// Call the displayProducts function to fetch and render products 
+displayProducts(querySnapshot);
+
+
+
 
 //adding new product to 'products' collection
 /*try {
